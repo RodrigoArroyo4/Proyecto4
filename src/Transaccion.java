040bf9e23dfefe36fac06989679d4508adafee28
RodrigoArroyo4 <rodrigoarroyo4@hotmail.com>
2019-03-23 18:10:18 -0500

    created Transacciones and Transacciones queries

---
+++ src/Transaccion.java
@@ -0,0 +1,48 @@
+public class Transaccion
+{
+
+    private String idCliente;
+    private String tipo;
+    private String valor;
+    private String fecha;
+
+    public Transaccion (String IdCliente, String Tipo, String Valor, String Fecha)
+    {
+        idCliente = IdCliente;
+        tipo = Tipo;
+        valor = Valor;
+        fecha = Fecha;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+}
