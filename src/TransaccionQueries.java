a6da5a86990f867e72c6edbdd6ea46d3c215916a
RodrigoArroyo4 <rodrigoarroyo4@hotmail.com>
2019-03-23 18:20:10 -0500

    created Transacciones and Transacciones queries

--- src/TransaccionQueries.java
+++ src/TransaccionQueries.java
@@ -1,4 +1,16 @@
+import java.sql.*;
+
 public class TransaccionQueries
 {
+    private static final String URL = "jdbc:derby:BancoAA";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private Connection connection;
+
+    public TransaccionQueries()
+    {
+        //consulta de saldos, consulta de transacciones, deposito y retiro
+    }
 
 }
