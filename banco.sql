710bf60f30d1c6651c84a0a03865081ca3652aaa
RodrigoArroyo4 <rodrigoarroyo4@hotmail.com>
2019-03-23 17:47:55 -0500

    created tables and added data into them

--- banco.sql
+++ banco.sql
@@ -1,27 +1,30 @@
+DROP SCHEMA root;
+
+CREATE SCHEMA root;
 
 DROP TABLE cuentas;
 DROP TABLE cliente;
 DROP TABLE transacciones;
 
 CREATE TABLE cliente (
-   cliente_id integer (20) NOT NULL,
+   cliente_id int NOT NULL,
    nombre_cliente varchar (20) NOT NULL,
    PRIMARY KEY (cliente_id)
 );
 
 CREATE TABLE cuentas (
-   cuenta_id integer NOT NULL,
-   cliente_id integer NOT NULL,
-   saldo money NOT NULL,
-   PRIMARY KEY (cuenta_id)
+   cuenta_id int NOT NULL,
+   cliente_id int NOT NULL,
+   saldo decimal (19,4) NOT NULL,
+   PRIMARY KEY (cuenta_id),
    FOREIGN KEY (cliente_id) REFERENCES cliente (cliente_id) ON DELETE CASCADE
 );
 
 CREATE TABLE transacciones (
-   transaccion_id integer NOT NULL,
-   cuenta_id integer NOT NULL,
+   transaccion_id int NOT NULL,
+   cuenta_id int NOT NULL,
    tipo varchar (100) NOT NULL,
-   valor money NOT NULL,
+   valor decimal (19,4) NOT NULL,
    fecha date NOT NULL,
    PRIMARY KEY (transaccion_id),
    FOREIGN KEY (cuenta_id) REFERENCES cuentas (cuenta_id) ON DELETE CASCADE
@@ -29,18 +32,18 @@ CREATE TABLE transacciones (
 
 INSERT INTO cliente (cliente_id, nombre_cliente) 
 VALUES
-('1716181654', 'Rodrigo Arroyo'), 
-('1716181655', 'Aaron Salazar');
+(1716181654, 'Rodrigo Arroyo'), 
+(1716181655, 'Aaron Salazar');
 
 INSERT INTO cuentas (cuenta_id, cliente_id, saldo) 
 VALUES
-('0001', '1716181654', '2000'), 
-('0002', '1716181655', '150');
+(0001, 1716181654, 2000), 
+(0002, 1716181655, 150);
 
 INSERT INTO transacciones (transaccion_id, cuenta_id, tipo, valor, fecha) 
 VALUES
-('0001', '0001', 'Deposito', 10, 2019-03-23), 
-('0002', '0002', 'Deposito', 100, 2019-03-22);
+(0001, 0001, 'Deposito', 10, '2019-03-23'), 
+(0002, 0002, 'Deposito', 100, '2019-03-22');
 
  
 
